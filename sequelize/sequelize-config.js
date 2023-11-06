70a1f0314cfffbb91084dd7636426a2d6244ce6f
RohendoJ <rohendoj3134.github@gmail.com>
2023-11-07 00:42:19 +0700

    feat: change db prod

--- sequelize/sequelize-config.js
+++ sequelize/sequelize-config.js
@@ -2,6 +2,4 @@ import { Sequelize } from "sequelize";
 
 const db = process.env.DB_URL + "?sslmode=require";
 
-export const sequelize = new Sequelize(
-  "postgresql://postgres:123@localhost:5432/db_mygram"
-);
+export const sequelize = new Sequelize(db);
